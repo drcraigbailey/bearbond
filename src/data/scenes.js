449aad9d37aa78a849b81b71e8e30f70630b0adf
drcraigbailey <drcraig.bailey@gmail.com>
2026-06-16 22:21:39 +0100

    Add shop and vets scene selections

--- src/data/scenes.js
+++ src/data/scenes.js
@@ -4,6 +4,8 @@ import bgUni from '../assets/scenes/backdrop-uni.png';
 import bgHill from '../assets/scenes/backdrop-hill.png';
 import bgWork from '../assets/scenes/backdrop-work.png';
 import bgPub from '../assets/scenes/backdrop-pub.png';
+import bgShop from '../assets/scenes/backdrop-shop.svg';
+import bgVets from '../assets/scenes/backdrop-vets.svg';
 
 // The "export const" is required here to match the import { SCENES } in your other files.
 export const SCENES = {
@@ -11,5 +13,7 @@ export const SCENES = {
   hill: { id: 'hill', name: 'Sunny Hills', image: bgHill },
   uni: { id: 'uni', name: 'University', image: bgUni },
   work: { id: 'work', name: 'Cafe / Work', image: bgWork },
+  shop: { id: 'shop', name: 'Shop', image: bgShop },
+  vets: { id: 'vets', name: 'Vets', image: bgVets },
   pub: { id: 'pub', name: 'The Golden Tankard', image: bgPub },
 };
