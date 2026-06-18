3cbf13889118558f7712acca14d95975e0f19fa1
drcraigbailey <drcraig.bailey@gmail.com>
2026-06-18 08:15:33 +0100

    Use night action id for night sound

--- src/data/actions.js
+++ src/data/actions.js
@@ -2,9 +2,9 @@ export const ACTIONS = [
   { id: 'wave', label: 'Wave', icon: '👋' },
   { id: 'love', label: 'Love', icon: '❤️' },
   { id: 'hug', label: 'Hug', icon: '🫂' },
-  { id: 'dance', label: 'Dance', icon: '🍑' }, 
-  { id: 'sleep', label: 'Sleep', icon: '💤' },
+  { id: 'dance', label: 'Dance', icon: '🍑' },
+  { id: 'night', label: 'Night', icon: '💤' },
   { id: 'honey', label: 'Honey', icon: '🍯' },
   { id: 'chicken', label: 'Chicken', icon: '🐔' },
-  { id: 'celebrate', label: 'Fancy', icon: '🎩' }, 
+  { id: 'celebrate', label: 'Fancy', icon: '🎩' },
 ];
