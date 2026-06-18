1301b15005399ba9fded02ff47a5fe0d5adef373
drcraigbailey <drcraig.bailey@gmail.com>
2026-06-18 10:56:11 +0100

    Add native BearBond widget Capacitor plugin

---
+++ android/app/src/main/java/com/bearbond/app/BearBondWidgetPlugin.java
@@ -0,0 +1,22 @@
+package com.bearbond.app;
+
+import com.getcapacitor.JSObject;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.annotation.CapacitorPlugin;
+import com.getcapacitor.annotation.PluginMethod;
+
+@CapacitorPlugin(name = "BearBondWidget")
+public class BearBondWidgetPlugin extends Plugin {
+    @PluginMethod
+    public void updateAction(PluginCall call) {
+        String actionId = call.getString("actionId", "");
+        String direction = call.getString("direction", "received");
+
+        BearBondActionWidgetProvider.updateAllWidgets(getContext(), actionId, direction);
+
+        JSObject response = new JSObject();
+        response.put("updated", true);
+        call.resolve(response);
+    }
+}
